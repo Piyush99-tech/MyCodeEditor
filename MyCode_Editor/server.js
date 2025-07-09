b59474effe4f00824991fb96fa3f3cce71e5a9cf
Piyush99-tech <piyushkala9@gmail.com>
2025-07-10 00:22:31 +0530

    Update server.js

--- MyCode_Editor/server.js
+++ MyCode_Editor/server.js
@@ -158,8 +158,7 @@ socket.on('disconnecting', () => {
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`server is running at ${PORT}`);
-<<<<<<< Updated upstream
+
 });
-=======
-});
->>>>>>> Stashed changes
+
+
