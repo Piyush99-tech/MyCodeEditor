c08f649ab78e0992a694f2e0e6314d53f80949d8
Piyush99-tech <piyushkala9@gmail.com>
2025-07-09 23:41:51 +0530

    Update Actions.js

--- MyCode_Editor/src/Actions.js
+++ MyCode_Editor/src/Actions.js
@@ -5,7 +5,10 @@ const ACTIONS = {
     CODE_CHANGE: 'code-change',
     SYNC_CODE: 'sync-code',
     LEAVE: 'leave',
-    LANGUAGE_CHANGE:'change'
+    LANGUAGE_CHANGE:'change',
+     CODE_EXECUTION: 'code-execution',
+  CODE_OUTPUT: 'code-output',
 };
 
 export default ACTIONS;
+
